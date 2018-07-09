2c1a3e0c47b675e4e958448dd07bbb20037fa7a6
zhuanglee <devlzh93@gmail.com>
2018-07-09 17:19:51 +0800

    Clean up ignored files

--- apl-tools/index.js
+++
@@ -1,4 +0,0 @@
-const requireFolder = require('apl-require-folder');
-const path = require('path');
-
-module.exports = requireFolder(path.join(__dirname, 'lib'));
--- apl-tools/lib/time-formator.js
+++
@@ -1,5 +0,0 @@
-const moment = require('moment');
-
-module.exports = function format(dateObj, style='YYYY-MM-DD HH:mm:ss') { 
-  return moment(dateObj).format(style);
-};
